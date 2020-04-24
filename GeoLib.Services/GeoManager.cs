95c9609c3ec68622a3cb3325e87ff8d98c1882fd
Robert Dannelly <robertdannelly@gmail.com>
2020-04-23 20:42:16 -0400

    GeoManager.cs adding Services for getting data

---
+++ GeoLib.Services/GeoManager.cs
@@ -0,0 +1,49 @@
+﻿using GeoLib.Contracts;
+using GeoLib.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoLib.Services
+{
+    public class GeoManager : IGeoService
+    {
+        public IEnumerable<string> GetStates(bool primaryOnly)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ZipCodeData GetZipInfo(string zip)
+        {
+            ZipCodeData zipCodeData = null;
+
+            IZipCodeRepository zipCodeRepository = new ZipCodeRepository();
+
+            ZipCode zipCodeEntity = zipCodeRepository.GetByZip(zip);
+
+            if (zipCodeEntity != null)
+            {
+                zipCodeData = new ZipCodeData()
+                {
+                    City = zipCodeEntity.City,
+                    State = zipCodeEntity.State.Abbreviation,
+                    ZipCode = zipCodeEntity.Zip
+                };
+            }
+
+            return zipCodeData;
+        }
+
+        public IEnumerable<ZipCodeData> GetZips(string state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ZipCodeData> GetZips(string state, int range)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
